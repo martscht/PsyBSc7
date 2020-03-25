740cd7f6d4280a4245aba294c0f12dde54268524
Martin Schultze <schultze@psych.uni-frankfurt.de>
2020-03-25 11:13:48 +0100

    Initial commit. Add ggplot2 exercise.

---
+++ R/PsyBSc7-package.R
@@ -0,0 +1,7 @@
+#' PsyBSc7: Übungen für die Praktika im Sommersemester 2020
+#'
+#' In diesem Paket werden die Übungen für die Praktika in PsyBSc 7 gebündelt. Die einzelnen Übungen können Sie über ihre jeweilige Funktion ansprechen.
+#'
+#' @docType package
+#' @name PsyBSc7
+#' @import shiny learnr ggplot2 devtools
---
+++ R/sitzungen.R
@@ -0,0 +1,13 @@
+#' Übungen für das Praktikum in PsyBSc 7, Sommersemester 2020
+#'
+#' Mit diesen Funktionen können Sie die Übungen zu den einzelnen Sitzungen aufrufen.
+#'
+#' @export
+
+#### Kernfunktionen: Aufruf der einzelnen Sitzungen ####
+Sitzung_2 <- function() {
+  if (!require(fontawesome)) {
+    devtools::install_github("rstudio/fontawesome")
+  }
+  learnr::run_tutorial('ggplot', package = 'PsyBSc7')
+}
