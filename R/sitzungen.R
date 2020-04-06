99784fa7260a81bf505c6e34ca099c14d2adb2c9
Martin Schultze <schultze@psych.uni-frankfurt.de>
2020-04-06 12:49:41 +0200

    Update documentation.

--- R/sitzungen.R
+++ R/sitzungen.R
@@ -1,10 +1,26 @@
-#' Übungen für das Praktikum in PsyBSc 7, Sommersemester 2020
+#' @name Sitzungen
+#' @aliases Sitzung_1
+#' @aliases Sitzung_2
+#' 
+#' @title Übungen für das Praktikum in PsyBSc 7, Sommersemester 2020
 #'
-#' Mit diesen Funktionen können Sie die Übungen zu den einzelnen Sitzungen aufrufen.
+#' Mit diesen Funktionen können Sie die Übungen zu den einzelnen Sitzungen aufrufen. Die Funktionen nehmen keine Argumente entgegen.
 #'
-#' @export
-
+  
 #### Kernfunktionen: Aufruf der einzelnen Sitzungen ####
+
+#' @rdname Sitzungen
+#' @export
+Sitzung_1 <- function() {
+  library(learnr)
+  if (!('fontawesome' %in% rownames(installed.packages()))) {
+    devtools::install_github("rstudio/fontawesome", upgrade = 'never')
+  }
+  learnr::run_tutorial('Wiederholung', package = 'PsyBSc7')
+}
+
+#' @rdname Sitzungen
+#' @export
 Sitzung_2 <- function() {
   library(learnr)
   if (!('fontawesome' %in% rownames(installed.packages()))) {
