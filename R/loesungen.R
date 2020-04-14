32f84dbee0010d2b05e969461aa1dd90ac2f2ec0
Martin Schultze <schultze@psych.uni-frankfurt.de>
2020-04-14 14:00:10 +0200

    Add solutions to Session 1.

---
+++ R/loesungen.R
@@ -0,0 +1,23 @@
+#' @name Lösungen
+#' @aliases Lösungen_1
+#' @aliases Lösungen_2
+#' 
+#' @title R-Skripte zur Lösung der Aufgaben im Praktikum zu PsyBSc 7, Sommersemester 2020
+#'
+#' Mit diesen Funktionen können Sie die Lösungen zu den Übungen in den einzelnen Sitzungen aufrufen. Die Passwörter werden nach dem Abgabetermin im OLAT bekannt gegeben.
+#'
+
+#### Kernfunktionen: Aufruf der einzelnen Sitzungen ####
+Loesungen_1 <- function(password) {
+  if (digest::digest(password) != '92195e95ce62da2d8a449c0739e459ca') {
+    stop('Leider haben Sie nicht das richtige Passwort eingegeben.', call. = FALSE)
+  }  else {
+    compile('intro_lo')
+  }
+}
+
+
+#### Kompilieren ####
+compile <- function(x) {
+  learnr::run_tutorial(x, package = 'PsyBSc7')
+}
