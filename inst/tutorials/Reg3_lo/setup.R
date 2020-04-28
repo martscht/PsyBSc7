bea173573da21824d511f668472a0836468e53b3
jpirmer <julienirmer_17@yahoo.de>
2020-04-28 11:32:16 +0200

    Add data set instructions.

--- inst/tutorials/Reg3_lo/setup.R
+++ inst/tutorials/Reg3_lo/setup.R
@@ -1,7 +1,8 @@
-#### Setup for 02_ggplot in learnr ####
+#### Setup for Reg3_lo in learnr ####
 
 ### Require packages ----
 library(fontawesome)
 library(ggplot2)
 library(learnr)
 library(gradethis)
+data("PISA2009", package = "PsyBSc7")
