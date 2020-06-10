8a12670820b917e0d4512591cfc57f6cbe25611e
Martin Schultze <schultze@psych.uni-frankfurt.de>
2020-06-10 20:51:39 +0200

    Finish ANOVA 2. Add nature dataset fpr exercise.

---
+++ inst/tutorials/anova2/startup.R
@@ -0,0 +1,47 @@
+#### Goethe Color Palette ----
+# thanks @ https://drsimonj.svbtle.com/
+# define colors
+goethe_colors <- c(`blue`='#00618f', 
+  `yellow` = '#e3ba0f', 
+  `magenta` = '#ad3b76', 
+  `green` = '#737c45', 
+  `orange` = '#c96215')
+
+# define extractor
+goethe_cols <- function(...) {
+  cols <- c(...)
+  if (is.null(cols)) return(goethe_colors)
+  else goethe_colors[cols]
+}
+
+# define palette
+goethe_palettes <- list(
+  `main` = goethe_cols('blue', 'yellow', 'magenta'),
+  `long` = goethe_cols()
+)
+
+# palette grabber
+goethe_pal <- function(palette = "main", reverse = FALSE, ...) {
+  pal <- goethe_palettes[[palette]]
+  if (reverse) pal <- rev(pal)
+  colorRampPalette(pal, ...)
+}
+
+# define scales for ggplot
+scale_color_goethe <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- goethe_pal(palette = palette, reverse = reverse)
+  if (discrete) {
+    discrete_scale("colour", paste0("goethe_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+scale_fill_goethe <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- goethe_pal(palette = palette, reverse = reverse)
+  if (discrete) {
+    discrete_scale("fill", paste0("goethe_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
